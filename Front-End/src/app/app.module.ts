ba6ffcd11bf528d0bff338051eaff1a83947c8c5
Diego Lalanda <guaio_2014@hotmail.com>
2023-06-15 20:19:12 -0300

    Inicio del BackEnd

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -24,10 +27,13 @@ import { FooterComponent } from './components/footer/footer.component';
     ProyectosComponent,
     ContactoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
