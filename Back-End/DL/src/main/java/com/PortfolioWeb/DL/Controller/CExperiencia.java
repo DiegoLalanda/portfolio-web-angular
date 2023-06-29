56ed0a9785f5fe6046731bf942c0a7260314f184
Diego Lalanda <guaio_2014@hotmail.com>
2023-06-29 19:10:18 -0300

    edito las url

--- Back-End/DL/src/main/java/com/PortfolioWeb/DL/Controller/CExperiencia.java
+++ Back-End/DL/src/main/java/com/PortfolioWeb/DL/Controller/CExperiencia.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://dlfrontend.web.app/")
 @RequestMapping("/explab")
 public class CExperiencia {
 
